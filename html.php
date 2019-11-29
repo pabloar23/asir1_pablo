3bb45d399bfc35c0b308d4cdba24fcc4e806983a
unknown <pablo.argumosa.ruiz@gmail.com>
2019-11-29 16:29:17 +0100

    Commit1

---
+++ html.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+		<title>LAS MARCAS MAS CARAS</title>
+		
+</head>
+<body bgcolor="blue">
+<font color="white">
+<center>
+<table width="80%" height="100%" bgcolor="white"
+<tr>
+	<td>
+		<center>
+		<h1>Marca 1</h1>
+		<hr>
+			<h2>Gucci</h2>
+				<p><b>Gucci</b> es una firma de lujo <b><u>italiana fundada en 1921</b></u> dedicada al <b><u>diseño y fabricación de artículos de moda, maletas, relojes, perfumes, etc.</u></b></br>
+						<table height="75"><tr><td></td></tr></table>
+			<h3>Lo más buscado</h3>
+				<p>Lo más buscado en nombre de la marca Gucci son bolsos, calcetines, camisetas y mochilas.</p>
+			<img
+			src="https://i1.wp.com/www.america-retail.com/static//2018/10/gucci.png?fit=1170%2C700&ssl=1"
+			align="center"
+			width="150"
+			height="100"
+			/>
+		</center>
+		<table height="75"><tr><td></td></tr></table>
+		<hr/>
+		<center>
+		<h1>Marca 2</h1>
+		<hr>
+		<h2>Louis Vuitton</h2>
+				<p><b>Louis Vuitton</b> casa <b><u>fundada en 1854</b></u>, es una empresa <b><u>francesa</b></u> de marroquinería de lujo <b><u>especializada en artículos de viaje que vende a través de boutiques independientes, departamentos en almacenes de alta gama y a través de la sección de comercio electrónico en su sitio web.</b></u></p>
+				<table height="75"><tr><td></td></tr></table>
+			<h3>Lo más buscado</h3>
+				<p>Lo más buscado de la marca Louis Vuitton son bolsos, maletas y carteras.</p>
+			<img
+			src="https://thumbor.forbes.com/thumbor/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ce2e16b169cc600076c55c4%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D416%26cropY1%3D0%26cropY2%3D416"
+			align="center"
+			width="150"
+			height="100"
+			/>
+		</center>
+			<table height="75"><tr><td></td></tr></table>
+			<hr/>
+		<center>
+		<h1>Marca 3</h1>
+		<hr>
+		<h2>Versace</h2>
+				<p><b>Versace</b>, es una <b><u>casa de moda Italiana, fundada por Gianni Versace en 1978.</b></u>
+			<table height="75"><tr><td></td></tr></table>
+			<h3>Lo más buscado</h3>
+			<p>Lo más buscado de la marca Versace son camisetas, ropa interior y vestidos.</p>
+			<img
+			src="versace.jpg"
+			align="center"
+			width="150"
+			height="100"
+			/>
+			<table height="100"><tr><td></td></tr></table>
+			<hr/>
+			
+		
+		<p>Le dejamos a continuación un formulario donde si se identifica con su <b>ID Cliente y su clave correspondiente</b> podrá iniciar sesión y valorar su experiencia en nuestra web, gracias de antemano.</p>
+		
+			<form>
+				ID Cliente:<br/>
+			<input type="text" name="username"><br/>
+				Clave:<br/>
+			<input type="password" name="psw"><br/>
+				Utilizar estas credenciales para iniciar sesión:<br/>
+			<input type="submit" value="Submit"><br/>
+			</form>
+			<hr/>
+			
+			<center>
+			<table width="80%" border >
+			<tr><td colspan="2" text align=center bgcolor="white"><b>Creado y editado por ASIR2Decroly S.L</b></td></tr>
+			<tr><td text align=center bgcolor="white"><b>Versión 8.4</b></td><td text align=center bgcolor="white" ><b>All rights reserved</b></td></tr>
+</tr>
+</table>
+</table>
+</center>
+</center>
+</body>
+
+
+		
